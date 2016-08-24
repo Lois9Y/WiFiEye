305de85f3aab777825ed6962cdd2c979026e577b
Lois9Y <alois.wollersberger@9yards.at>
2016-08-24 10:46:21 +0200

    Added history Tab.
    Some minor polish

---
+++ app/src/main/java/at/a9yards/wifieye/ScannedNetworksAdapter.java
@@ -0,0 +1,77 @@
+package at.a9yards.wifieye;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.graphics.PorterDuff;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import at.a9yards.wifieye.data.NetworkItem;
+import io.realm.OrderedRealmCollection;
+import io.realm.Realm;
+import io.realm.RealmBaseAdapter;
+
+/**
+ * Created by Lois-9Y on 24/08/2016.
+ */
+public class ScannedNetworksAdapter extends RealmBaseAdapter<NetworkItem> implements ListAdapter {
+
+    private final String LOG_TAG = ScannedNetworksAdapter.class.getSimpleName();
+
+    public static class ViewHolder {
+
+        final TextView ssidTextView;
+        final ImageButton deleteButton;
+
+        public ViewHolder(View container) {
+            this.ssidTextView = (TextView) container.findViewById(R.id.history_net_ssid);
+            this.deleteButton = (ImageButton) container.findViewById(R.id.history_net_delete);
+        }
+    }
+
+    public ScannedNetworksAdapter(@NonNull Context context, @Nullable OrderedRealmCollection<NetworkItem> data) {
+        super(context, data);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null){
+            Log.d(LOG_TAG,"init viewholder ");
+            convertView = inflater.inflate(R.layout.history_list_item,parent,false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        NetworkItem item = adapterData.get(position);
+        viewHolder.ssidTextView.setText( item.getSSID());
+
+        viewHolder.deleteButton.setImageResource(R.drawable.ic_clear_black_24dp);
+        viewHolder.deleteButton.getDrawable().setColorFilter(convertView.getResources().getColor(R.color.icons), PorterDuff.Mode.SRC_ATOP);
+
+        viewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Realm realm = Realm.getDefaultInstance();
+                realm.beginTransaction();
+                realm.where(NetworkItem.class).equalTo(NetworkItem.FIELDNAME_SSID,adapterData.get(position).getSSID()).findAll().deleteAllFromRealm();
+                realm.commitTransaction();
+                ScannedNetworksAdapter.this.notifyDataSetChanged();
+            }
+        });
+
+        return convertView;
+    }
+}
--- app/src/main/java/at/a9yards/wifieye/ScannedNetworksFragment.java
+++ app/src/main/java/at/a9yards/wifieye/ScannedNetworksFragment.java
@@ -3,18 +3,53 @@ package at.a9yards.wifieye;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.swiperefreshlistfragment.SwipeRefreshListFragment;
+
+import at.a9yards.wifieye.data.NetworkItem;
+import io.realm.Realm;
+import io.realm.Sort;
+
 /**
  * Created by Lois-9Y on 10/08/2016.
  */
-public class ScannedNetworksFragment extends Fragment{
-    @Nullable
+public class ScannedNetworksFragment extends SwipeRefreshListFragment {
+    ScannedNetworksAdapter mAdapter;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.tab_history_xml,container,false);
-        return root;
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Realm realm = Realm.getDefaultInstance();
+
+        if (mAdapter == null) {
+            mAdapter = new ScannedNetworksAdapter(getActivity(), realm.where(NetworkItem.class).findAll());
+            setListAdapter(mAdapter);
+            mAdapter.updateData(realm.where(NetworkItem.class)
+                    .isNotNull(NetworkItem.FIELDNAME_PASSWORD)
+                    .notEqualTo(NetworkItem.FIELDNAME_PASSWORD, "")
+                    .findAllSorted(NetworkItem.FIELDNAME_SSID, Sort.ASCENDING));
+        }
+
+
+        setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initiateRefresh();
+            }
+        });
+
+
+        //Log.d(LOG_TAG,"onCreate done")
     }
+    private void initiateRefresh() {
+        //Lets see what TODO here
+        setRefreshing(false);
+    }
+
 }
